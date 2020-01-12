f186b06e67ee763ebbb54a456fed858af720c519
Me <ivo@localhost.localdomain>
2020-01-12 04:46:20 -0800

    clean

--- modexp/target/debug/deps/modexp-2e64388c3b674fbf.d
+++
@@ -1,5 +0,0 @@
-/home/ivo/cs/rust/modexp/target/debug/deps/modexp-2e64388c3b674fbf: src/main.rs
-
-/home/ivo/cs/rust/modexp/target/debug/deps/modexp-2e64388c3b674fbf.d: src/main.rs
-
-src/main.rs:
--- modexp/target/debug/modexp.d
+++
@@ -1,1 +0,0 @@
-/home/ivo/cs/rust/modexp/target/debug/modexp: /home/ivo/cs/rust/modexp/src/main.rs
