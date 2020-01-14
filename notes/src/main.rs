8125d31b84764f9f9e3e74c7777b0d4fb2a48ae8
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-01-13 18:31:35 -0800

    more notes

--- notes/src/main.rs
+++ notes/src/main.rs
@@ -1,17 +1,57 @@
-use std::io::BufRead;
+#[derive(Debug)]
+pub enum Color {
+	Red,
+	Green,
+	Blue,
+	Custom(f32, f32, f32),
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Point {
+	x: u64,
+	y: u64,
+}
+
+impl Point {
+	fn swp(&mut self) {
+		self.x += self.y;
+		self.y += self.x;
+	}
+
+	fn show_point(&self) {
+		println!("{:?}", self);
+	}
+}
+
+fn pattern_match(s: Color) {
+	if let Color::Custom(_, g, _) = s {
+		println!("{:?}", g);
+	}
+}
+
+fn nums_1() {
+	let mut x = 5.012f32;
+	let y = (1, 'a', false);
+	let z = x;
+	x += 1.0;
+	println!("{} {:?} {}", x, y, z);
+}
 
 fn main() {
-    let x = 1; // Local variables
+	nums_1();
 
-    let stdin = std::io::stdin(); // Standard input buffer
-    let mut sum = 0;
+	let mut p = Point { x:12, y: 20 };
+	p.show_point();
+	p.swp();
+	p.show_point();
+	let t = p.clone();
+	t.show_point();
 
-    for line in stdin.lock().lines() {
-        // Logic block
-        let line = line.expect("io error");
-        let val: i64 = line.parse().expect(" invalid number");
-        sum += val;
-    }
+	let c1: Color = Color::Red;
+	let c2: Color = Color::Green;
+	let c3: Color = Color::Blue;
+	let c4: Color = Color::Custom(0.8, 0.7, 0.6);
+	println!("{:?} {:?} {:?}\n", c1, c2, c3);
 
-    println!("{}", sum);
+	pattern_match(c4);
 }
