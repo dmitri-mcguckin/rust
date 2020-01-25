9b864e95310e62fc360417ce5f6adba242a0a07b
Me <ivo@localhost.localdomain>
2020-01-25 02:09:18 -0800

    added itterative rng tests and manual cli-based demo

--- rsa/examples/demo.rs
+++ rsa/examples/demo.rs
@@ -1,15 +1,26 @@
 use rsa::*;
-use rand::Rng;
+use std::env::*;
+use std::process;
 
 fn main() {
-	let (p, q) = genkey();
-	println!("Private Key: p = 0x{:x} q = 0x{:x}", p, q);
+    // Gather command-line arguments
+    let args: Vec<String> = args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run --example demo <message>");
+        process::exit(-1);
+    }
 
-	let modulo: u64 = u32_u64(p) * u32_u64(q);
-	println!("Public Key: p * q = 0x{:x}", modulo);
+    let (p, q) = genkey();
+    println!("Private Key: p = 0x{:x} q = 0x{:x}", p, q);
 
-    let request = rand::thread_rng().gen_range(0,10000);
-	let cipher = encrypt(modulo, request);
-	let response = decrypt((p, q), cipher);
-	println!("Message: {}\nEncrypted: 0x{:x}\nDecrypted: {}", request, cipher, response);
+    let modulo: u64 = u32_u64(p) * u32_u64(q);
+    println!("Public Key: p * q = 0x{:x}", modulo);
+
+    let request = *&args[1].trim().parse::<u32>().unwrap();
+    let cipher = encrypt(modulo, request);
+    let response = decrypt((p, q), cipher);
+    println!(
+        "Message: {}\nEncrypted: 0x{:x}\nDecrypted: {}",
+        request, cipher, response
+    );
 }
--- rsa/tests/rng_tests.rs
+++ rsa/tests/rng_tests.rs
@@ -1,4 +1,15 @@
+use rand::Rng;
+use rsa::*;
+
 #[test]
-fn test_1() {
-	assert!(false);
+fn test() {
+    for i in 0..1000 {
+        println!("iterating test #{}", i);
+        let (p, q) = genkey();
+        let modulo: u64 = u32_u64(p) * u32_u64(q);
+        let request = rand::thread_rng().gen_range(0, 10000);
+        let cipher = encrypt(modulo, request);
+        let response = decrypt((p, q), cipher);
+        assert!(request == response);
+    }
 }
